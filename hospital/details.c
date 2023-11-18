db44c397184c3f921ee7de829f23fee8144ffdf4
root <root@DESKTOP-AL6U6H1>
2023-11-18 21:12:24 +0300

    1000_steps_to_the_valley

--- hospital/details.c
+++
@@ -1,9 +0,0 @@
-#include "hosp.h"
-int print_details(int argc, const char * argv[])
-{
-	int i, ch, num;
-	int choice, update, updateChoice;
-	struct patient *e1;
-	printf("Enter the patient's number");
-	puts("%d", &num);
-	e1 = malloc (sizeof(struct patient) * num);
